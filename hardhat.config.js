8380ec96ba93707bb8a8ea61d3815ddf8a1f8962
Evert Kors <evert@hatching.io>
2021-01-05 22:02:39 +0100

    yarn update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-web3");
+//require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,4 +21,3 @@ task("accounts", "Prints the list of accounts", async () => {
 module.exports = {
   solidity: "0.7.4",
 };
-
